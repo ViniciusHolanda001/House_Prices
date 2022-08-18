9ce6af5b9a6e563805511c5a214495245b7411b2
Vinicius Holanda Cavalcante <viniciusholandacavalcante@gmail.com>
2022-08-17 21:48:06 -0300

    Only function

--- test/run_pytest.py
+++ test/run_pytest.py
@@ -2,9 +2,6 @@ from pyexpat import model
 import pytest
 
 
-class Test():
-
-    @classmethod
-    def setup_model(cls):
-        model = object
-        assert model == object
+def setup_model(cls):
+    model = object
+    assert model == object

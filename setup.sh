8f662f35ef8bc077a6cad0b36668868cb596a497
Vinicius Holanda Cavalcante <viniciusholandacavalcante@gmail.com>
2022-09-16 21:45:19 -0300

    Setup dependencies

--- setup.sh
+++ setup.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+for i in 1 2 3; do
+  echo "counter: $i"
+done
+
+filename='environment.yml'
+n=1
+while read line; do
+# reading each line
+echo "$line"
+n=$((n+1))
+done < $filename
+
+while read requirement; do 
+    conda install --yes $requirement || pip install $requirement;
+    done < 'environment.yml' 
